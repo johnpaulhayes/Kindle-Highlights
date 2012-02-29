77f0cf5e93b5a50880032b3bac92401d1c7a0b4d
John Paul Hayes <johnpaul@webdeveloper.cx>
2012-02-29 21:14:14 +0000

    updated database schema

--- resources/db/kindle_highlights.sql
+++ resources/db/kindle_highlights.sql
@@ -1,26 +1,16 @@
+CREATE DATABASE kindle_highlights;
+
 CREATE  TABLE `kindle_highlights`.`new_table` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `book_id` INT(4) NULL ,
   `high_lighted_text` TEXT NULL ,
-  `tweeted` INT(1) NULL ,
+  `twitter` INT(1) NULL ,
+  `facebook` INT(1) NULL ,
   PRIMARY KEY (`id`) );
 	
 CREATE  TABLE `kindle_highlights`.`kindle_books` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `title` TEXT NULL ,
   `author` VARCHAR(254) NULL ,
-  `isbn` VARCHAR(15) NULL ,
+  `link` VARCHAR(254) NULL ,
   PRIMARY KEY (`id`) );
-	
-	
-pseudo code.
-
-populate form variables
-input field email,
-checkbox named create
-submit via post to 
-https://www.amazon.com/ap/signin
-There are others, but for the minute lets see if these work
-
-PHP
-
